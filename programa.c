4991bf08d86db28ee6f570c3d1ef6e0933790af8
violetttttttt <violeta.caride@alumnos.upm.es>
2022-04-06 18:11:22 +0200

    MENU DE OPCIONES TERMINADO

--- programa.c
+++ programa.c
@@ -6,7 +6,7 @@ struct TJugador {
 };
 int main () {
     int i=0;
-    int opcion;
+    int opcion, seguro;
     struct TJugador Jugadores[200];//Se guarda espacio para 200 jugadores
 
     printf("               .....       ....     ..............    ...............   ...............   .....             .....     .....      .....     \n ");
@@ -42,8 +42,34 @@ int main () {
     printf("                   Le advertimos que debe pensar bien sus respuestas, porque si falla mas de tres preguntas, se cerrara el juego y nos quedaremos con su cuenta para siempre.\n");
     printf("                   Podra salir del juego en todo momento, pero una vez que lo haga, no habra vuelta atras.\n");
 
-
+    do {
     printf("\n                   Introduzca la opcion deseada:\n                   1) Modo facil\n                   2) Modo dificil\n                   3) Salir del juego\n");
+    scanf("%d", &opcion);
+            switch (opcion){
+        case 1 :
+            printf("\n                   Ha elegido el modo facil\n");
+            // En este modo de juego, habrá 7 preguntas tipo test
+            break;
+        case 2 :
+            printf("                   Ha elegido el modo dificil\n");
+            //En este modo habrá 10 preguntas
+            break;
+        case 3 :
+            printf("                   Va a perder su cuenta de netflix, ¿esta seguro que quiere salir?\n                    1) Si\n                    2) No\n");
+            scanf("%d", &seguro);
+                switch (seguro){
+                    case 1 :
+                    printf ("                   Ha perdido su cuenta de netflix.\n");
+                    break;
+                    case 2 :
+                    break;
+                    //Que vuelva introducir al menu de facil/dificil/salir
+                }
+
+
+            break;
+    }
+    } while (opcion==3 &&seguro==2);
 
 
 
