65c768c272e2d9891d1771d753f3aec1a0538087
VictoriaLopezLorenzo <100776063+VictoriaLopezLorenzo@users.noreply.github.com>
2022-04-06 15:59:10 +0200

    Inicio menú

--- programa.c
+++ programa.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
+//Declaración del vector de estructura
+struct TJugador {
+    char NombreUsuario[50];
+    char Contraseña[50];
+};
 int main () {
+    struct TJugador Jugadores[200];//Se guarda espacio para 200 jugadores
+    int
 
 
 }
